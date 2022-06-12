d880f568ef12a89f2bb0b0e0854b0ca4ccceb7f4
hack4peace <90603785+official-hack4peace@users.noreply.github.com>
2022-06-12 19:37:51 +0530

    Update ddos-mrx.py

--- ddos-mrx.py
+++ ddos-mrx.py
@@ -14,7 +14,7 @@ os.system("clear"
 print (Fore.GREEN + " ___  ___  ___  ___") 
 print ("             | . \| . \| . |/ __>")
 print ("             | | || | || | |\__ \")
-print ("             |___/|___/`___'<___/")
+print ("             |___/|___/`___|<___/")
       
 print "                 Created by: Mr_x"
 print "     Github: https://github.com/official-hack4peace"
